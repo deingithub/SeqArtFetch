6be219fb203632ee59c14b45fe6e2c369d1f0217
Cassidy Dingenskirchen <admin@15318.de>
2019-11-15 22:52:28 +0100

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    (pkgs.python3.withPackages(
+      ps: with ps; [ black pylint lxml cssselect aiohttp ]
+    )) 
+  ];
+
+  # For VSCode, easily-accessible symlinks to the nix-shell's linter/formatter
+  shellHook = ''
+  rm ./nix-shell-black 2> /dev/null
+  rm ./nix-shell-pylint 2> /dev/null
+  ln -s `which black` ./nix-shell-black 2> /dev/null
+  ln -s `which pylint` ./nix-shell-pylint 2> /dev/null
+  '';
+}
